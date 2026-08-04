08d44a803370aa6aafe730101d2986d4e0fe2a9e
rezaluthfi <rezaluthfiakbar7@gmail.com>
2026-08-04 18:04:31 +0700

    fix: hardcode explicit projectId fallback in sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,8 +6,8 @@ import { projectId, dataset } from './sanity/env';
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
+  projectId: projectId || 'xsj3gs4e',
+  dataset: dataset || 'production',
   title: 'Kabola Digital Hub Studio',
 
   plugins: [
